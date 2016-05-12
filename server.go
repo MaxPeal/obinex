33509821f547b281adbbd05163e834459d267f41
Lukas Senger <lukas@fridolin.com>
2016-05-12 13:48:13 +0200

    implement serial communication

--- server.go
+++ server.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"io"
 	"log"
 	"net/http"
 	"os"
 )
 
+import (
+	"github.com/tarm/serial"
+)
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Path: %s\n", r.URL.Path[1:])
+	log.Printf("Server: requested path: %s\n", r.URL.Path[1:])
 	f, err := os.Open("../octopos-jenkins/testcase.microbench")
 	if err != nil {
 		//todo
@@ -20,11 +25,44 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		//todo
 		panic(err)
 	}
-	log.Printf("served\n")
+	log.Printf("Server: binary served\n")
+}
+
+func getOutput(c chan string) {
+	conf := &serial.Config{
+		Name:   "/dev/ttyS0",
+		Baud:   115200,
+		Parity: serial.ParityNone,
+		Size:   8,
+	}
+	s, err := serial.OpenPort(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer s.Close()
+
+	r := bufio.NewReader(s)
+	for err == nil {
+		var line string
+		line, err = r.ReadString('\n')
+		c <- line
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleOutput(c chan string) {
+	for line := range c {
+		log.Printf("Output: %s", line)
+	}
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	log.Println("running")
+	log.Println("Server: running")
+	c := make(chan string, 10)
+	go getOutput(c)
+	go handleOutput(c)
 	http.ListenAndServe(":12334", nil)
 }
