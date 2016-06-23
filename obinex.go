deba0671b6835f2cdfd4f53a2a3a4d7184a89027
Lukas Senger <lukas@fridolin.com>
2016-06-23 16:16:22 +0200

    save output in file

--- obinex.go
+++ obinex.go
@@ -1,24 +1,37 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/rpc"
+	"os"
+	"path/filepath"
+	"time"
 )
 
 import (
 	"github.com/fsnotify/fsnotify"
 )
 
-const watchDir = "/proj/i4invasic/obinex"
+const watchDir = "/proj/i4invasic/obinex/"
 
 func run(client *rpc.Client, bin string) string {
 	var res string
 	err := client.Call("Rpc.Run", bin, &res)
 	if err != nil {
-		log.Fatal("rpc error:", err)
+		log.Fatal("RPC Error:", err)
 	}
 	return res
+}
+
+func handleOutput(box, bin, s string) {
+	t := time.Now().Format("_2006_01_02_15_04")
+	f, err := os.Create(watchDir + "/" + box + "/out/" + bin + t + ".txt")
+	if err != nil {
+		log.Println("Output Error:", err)
+		return
+	}
+	defer f.Close()
+	f.WriteString(s)
 }
 
 func watchAndRun(name string) {
@@ -43,9 +56,9 @@ func watchAndRun(name string) {
 		select {
 		case event := <-watcher.Events:
 			if event.Op&fsnotify.Create == fsnotify.Create {
-				log.Println("Watcher: ", event.Name)
+				log.Println("Watcher:", event.Name)
 				s := run(client, event.Name)
-				fmt.Printf("%s", s)
+				handleOutput(name, filepath.Base(event.Name), s)
 			}
 		case err := <-watcher.Errors:
 			log.Println("fsnotify error:", err)
