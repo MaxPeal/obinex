7981f3c4e8e650174dc696f302e04db8b33ffbfa
Lukas Senger <lukas@fridolin.com>
2016-11-23 17:10:20 +0100

    Update mapping of buddy boxes to hw boxes

--- common.go
+++ common.go
@@ -2,12 +2,9 @@ package obinex
 
 // ControlHosts contains the mapping of buddy hostname to hardware box hostname.
 var ControlHosts map[string]string = map[string]string{
-	"faui49jenkins12":                            "faui49big01",
-	"faui49jenkins12.informatik.uni-erlangen.de": "faui49big01",
-	"faui49jenkins13":                            "faui49big02",
-	"faui49jenkins13.informatik.uni-erlangen.de": "faui49big02",
-	"faui49jenkins14":                            "faui49big03",
-	"faui49jenkins14.informatik.uni-erlangen.de": "faui49big03",
-	"faui49jenkins21":                            "faui49jenkins25",
-	"faui49bello2":                               "fastbox",
+	"faui49jenkins12": "faui49big01",
+	"faui49jenkins13": "faui49big02",
+	"faui49jenkins14": "faui49big03",
+	"faui49jenkins15": "fastbox",
+	"faui49jenkins21": "faui49jenkins25",
 }
