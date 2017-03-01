1a2d59a78e00119cf360d2e7bcd5cd1a30b18b42
Lukas Senger <lukas@fridolin.com>
2017-03-02 00:34:45 +0100

    Remove use of time.Until as it is too new

--- obinex-watcher/lock.go
+++ obinex-watcher/lock.go
@@ -60,7 +60,7 @@ func (l *Lock) Set() error {
 	l.Expires = date
 	l.set = true
 	go func() {
-		c := time.After(time.Until(l.Expires))
+		c := time.After(l.Expires.Sub(time.Now))
 		<-c
 		l.set = false
 		os.Remove(l.Path)
