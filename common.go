66c6a7ec636aea728f7f53ea55adfb84fbaedecd
Lukas Senger <lukas@fridolin.com>
2017-03-29 21:06:56 +0200

    Add waiting mechanic to Watcher

--- common.go
+++ common.go
@@ -30,11 +30,21 @@ func changeStateOnPath(path, state string) string {
 
 func (wp *WorkPackage) ToQueued() error {
 	// Set the checksum
+	// Sometimes there is a delay before we can access the file via NFS, so
+	// we wait up to a second before erroring out.
 	f, err := os.Open(wp.Path)
-	if err != nil {
-		return err
+	i := 10
+	for err != nil {
+		i -= 1
+		if i == -1 {
+			break
+		}
+		log.Printf("Watcher: file access problem, retrying...\n")
+		time.Sleep(100 * time.Millisecond)
+		f, err = os.Open(wp.Path)
 	}
 	defer f.Close()
+
 	h := md5.New()
 	_, err = io.Copy(h, f)
 	if err != nil {
