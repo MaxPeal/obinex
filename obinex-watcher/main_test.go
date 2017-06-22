eeeddf09ae897a8ff17eb21f754f3a2bf7fdea4a
Lukas Senger <lukas@fridolin.com>
2017-06-22 14:58:58 +0200

    Test SetBootMode
    This uses the new Locker interface.

---
+++ obinex-watcher/main_test.go
@@ -0,0 +1,61 @@
+// +build !integration
+
+package main
+
+import (
+	"testing"
+
+	o "gitlab.cs.fau.de/luksen/obinex"
+)
+
+type MockLock struct {
+	Block bool
+}
+
+func (l *MockLock) Init(buddy *Buddy)  {}
+func (l MockLock) GetPath() string     { return "mocklocknopath" }
+func (l MockLock) Get(bin string) bool { return !l.Block }
+func (l *MockLock) Set() error         { return nil }
+func (l MockLock) Unset()              {}
+func (l MockLock) IsSet() bool         { return l.Block }
+
+func TestSetBootMode(t *testing.T) {
+	oldExecCommand := o.ExecCommand
+	defer func() { o.ExecCommand = oldExecCommand }()
+
+	buddy := Buddy{
+		Boxname: "testbox",
+		Lock:    &MockLock{},
+	}
+
+	called := false
+	o.ExecCommand = func(cmd string, args ...string) ([]byte, error) {
+		called = true
+		return nil, nil
+	}
+
+	buddy.SetBootMode("foo")
+	if called {
+		t.Error("ExecCommand called, should be invalid mode.")
+	}
+
+	buddy.Lock.(*MockLock).Block = true
+	buddy.SetBootMode("linux")
+	if called {
+		t.Error("ExecCommand called, should be blocked by lock.")
+	}
+
+	o.ExecCommand = func(cmd string, args ...string) ([]byte, error) {
+		if cmd == "bash" &&
+			args[0] == "-c" &&
+			args[1] == o.BootModePath+" testbox linux" {
+			called = true
+		}
+		return nil, nil
+	}
+	buddy.Lock.(*MockLock).Block = false
+	buddy.SetBootMode("linux")
+	if !called {
+		t.Error("ExecCommand called, expected no call.")
+	}
+}
