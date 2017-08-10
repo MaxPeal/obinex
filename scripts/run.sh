ed5e2ec3da0f871193a8bddc56795a10738c2f75
Lukas Senger <lukas@fridolin.com>
2017-08-10 17:22:07 +0200

    Temporarily disable big01

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-boxes="faui49big01,faui49big02,faui49big03,fastbox"
+boxes="faui49big02,faui49big03,fastbox"
 
 murder() {
 	sudo -u i4obinex ssh $1 killall $2
@@ -16,6 +16,6 @@ murder faui49jenkins25 obinex-server
 run faui49jenkins25 "obinex-server -box fastbox -serialpath /dev/ttyS7" fastbox
 run faui49jenkins25 "obinex-server -box faui49big03 -serialpath /dev/ttyS6" big03
 run faui49jenkins25 "obinex-server -box faui49big02 -serialpath /dev/ttyS5" big02
-run faui49jenkins25 "obinex-server -box faui49big01 -serialpath /dev/ttyS4" big01
+#run faui49jenkins25 "obinex-server -box faui49big01 -serialpath /dev/ttyS4" big01
 sleep 2
 run i4jenkins "obinex-watcher -host faui49jenkins25" watcher
