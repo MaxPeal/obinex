6e2ad6509e8a5554a267b94ade9950a69a102158
Lukas Senger <lukas@fridolin.com>
2017-08-10 15:54:17 +0200

    Try adapting run.sh to new infrastructure

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-servers="faui49jenkins12,faui49jenkins13,faui49jenkins14,faui49jenkins15"
+boxes="faui49big01,faui49big02,faui49big03,fastbox"
 
 murder() {
 	sudo -u i4obinex ssh $1 killall $2
 }
 
 run() {
-	sudo -u i4obinex ssh $1 "sh -c 'cd /proj/i4obinex/system/; nohup bin/$2 -servers $servers > /dev/null 2>$3.log &'"
+	sudo -u i4obinex ssh $1 "sh -c 'cd /proj/i4obinex/system/; nohup bin/$2 -boxes $boxes > /dev/null 2>$3.log &'"
 }
 
 murder i4jenkins obinex-watcher
-murder faui49jenkins12 obinex-server
-murder faui49jenkins13 obinex-server
-murder faui49jenkins14 obinex-server
-murder faui49jenkins15 obinex-server
+murder faui49jenkins25 obinex-server
+murder faui49jenkins25 obinex-server
+murder faui49jenkins25 obinex-server
+murder faui49jenkins25 obinex-server
 
-run faui49jenkins15 "obinex-server" fastbox
-run faui49jenkins14 "obinex-server" big03
-run faui49jenkins13 "obinex-server" big02
-run faui49jenkins12 "obinex-server" big01
+run faui49jenkins25 "obinex-server -box fastbox -serialpath /dev/ttyS7" fastbox
+run faui49jenkins25 "obinex-server -box faui49big03 -serialpath /dev/ttyS6" big03
+run faui49jenkins25 "obinex-server -box faui49big02 -serialpath /dev/ttyS5" big02
+run faui49jenkins25 "obinex-server -box faui49big01 -serialpath /dev/ttyS4" big01
 sleep 2
-run i4jenkins "obinex-watcher" watcher
+run i4jenkins "obinex-watcher -host faui49jenkins25" watcher
