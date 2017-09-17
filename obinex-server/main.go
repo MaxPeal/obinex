7eb33348ce74ad9720e94927421a2415d6ed4668
Lukas Senger <lukas@fridolin.com>
2017-09-17 16:02:57 +0200

    Output as string

--- obinex-server/main.go
+++ obinex-server/main.go
@@ -230,7 +230,7 @@ func handleOutput(c chan string) {
 func updateBootMode() {
 	for {
 		output, err := o.ExecCommand("nc", "-w", "2", "-zv", Boxname, "22")
-		log.Println("Bootmode:", output)
+		log.Println("Bootmode:", string(output))
 		if err == nil {
 			persistentWebData.Mode = "batch"
 		} else {
