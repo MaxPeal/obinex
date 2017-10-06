a7be3eb60ab111fa7fc6681e3ef911487b3f1cc5
Florian Schmaus <flo@geekplace.eu>
2017-10-06 13:53:03 +0200

    Improve scripts/update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-set -e                                                                          
-                                                                                
-shell="sudo -E -u i4obinex bash -c"                                             
-                                                                                
-cd src/gitlab.cs.fau.de/luksen/obinex/                                          
-$shell "git pull"                                                               
+set -e
+
+shell="sudo -E -u i4obinex bash -c"
+
+cd src/gitlab.cs.fau.de/i4/obinex/
+$shell "git pull"
 $shell "git submodule update --init --recursive"
-export GOPATH=/proj/i4obinex/system                                             
+export GOPATH=/proj/i4obinex/system
 $shell "go install gitlab.cs.fau.de/i4/obinex/..."
