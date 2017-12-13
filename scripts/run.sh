9b88e072cfb06025a02b09908705a0f2cd2892f2
Lukas Senger <lukas@fridolin.com>
2017-12-13 01:17:39 +0100

    Move some settings from run.sh to config

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-boxes="faui49big01,faui49big02,faui49big03,fastbox"
-
 murder() {
 	sudo -u i4obinex ssh -o StrictHostKeyChecking=no $1 killall $2
 }
 
 run() {
-	sudo -u i4obinex ssh -o StrictHostKeyChecking=no $1 "sh -c 'cd /proj/i4obinex/system/; nohup bin/$2 -boxes $boxes > /dev/null 2>>$3.log &'"
+	sudo -u i4obinex ssh -o StrictHostKeyChecking=no $1 "sh -c 'cd /proj/i4obinex/system/; nohup bin/$2 > /dev/null 2>>$3.log &'"
 }
 
 murder i4jenkins obinex-watcher
 murder faui49obinex obinex-server
 sleep 5
 
-run faui49obinex "obinex-server -box fastbox -serialpath /dev/ttyS7" fastbox
-run faui49obinex "obinex-server -box faui49big03 -serialpath /dev/ttyS6" big03
-run faui49obinex "obinex-server -box faui49big02 -serialpath /dev/ttyS5" big02
-run faui49obinex "obinex-server -box faui49big01 -serialpath /dev/ttyS4" big01
+run faui49obinex "obinex-server -box fastbox" fastbox
+run faui49obinex "obinex-server -box faui49big03" big03
+run faui49obinex "obinex-server -box faui49big02" big02
+run faui49obinex "obinex-server -box faui49big01" big01
 sleep 2
 run i4jenkins "obinex-watcher -host faui49obinex" watcher
