4bdf2f28824cb40c4ce2c6ebaf7889cea4fd7b46
Florian Schmaus <flow@cs.fau.de>
2020-02-18 11:10:51 +0100

    Change watcherhost from i4jenkins to faui49obinex

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,7 +8,7 @@ run() {
 	sudo -u i4obinex ssh -o StrictHostKeyChecking=no $1 "sh -c 'cd /proj/i4obinex/system/; nohup bin/$2 > /dev/null 2>>$3.log &'"
 }
 
-murder i4jenkins obinex-watcher
+murder faui49obinex obinex-watcher
 murder faui49obinex obinex-server
 sleep 5
 
@@ -17,4 +17,4 @@ run faui49obinex "obinex-server -box faui49big03" big03
 run faui49obinex "obinex-server -box faui49big02" big02
 run faui49obinex "obinex-server -box faui49big01" big01
 sleep 2
-run i4jenkins "obinex-watcher -host faui49obinex" watcher
+run faui49obinex "obinex-watcher -host faui49obinex" watcher
